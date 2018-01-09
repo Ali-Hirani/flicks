34229f531fd8f9d8443e67a8ac34df46786dc80f
Ali Hirani <ahirani@03413a-ahirani.corp.zynga.com>
2018-01-09 17:15:25 -0500

    Working implementation. TODO: Add new resource for horizontal layout.

--- app/src/main/java/com/example/ahirani/flicks/ui/FlicksActivity.java
+++ app/src/main/java/com/example/ahirani/flicks/ui/FlicksActivity.java
@@ -1,22 +1,38 @@
 package com.example.ahirani.flicks.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.example.ahirani.flicks.R;
 import com.example.ahirani.flicks.ui.models.MovieDBService;
 import com.example.ahirani.flicks.ui.models.MovieResponse;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class FlicksActivity extends AppCompatActivity {
+    @BindView(R.id.my_recycler_view)
+    RecyclerView recyclerView;
+    private FlicksAdapter adapter;
+    private boolean isLandscape = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flicks);
+        ButterKnife.bind(this);
+
+        Log.d("tag", "onCreate");
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
 
         MovieDBService service = MovieDBService.retrofit.create(MovieDBService.class);
         final Call<MovieResponse> call = service.getMovies();
@@ -26,7 +42,8 @@ public class FlicksActivity extends AppCompatActivity {
             public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
                 MovieResponse movieResponse = response.body();
                 if (movieResponse != null) {
-                    System.out.println(movieResponse.getMovies().get(0).getTitle());
+                    adapter = new FlicksAdapter(movieResponse.getMovies(), isLandscape);
+                    recyclerView.setAdapter(adapter);
                 }
             }
 
@@ -35,6 +52,16 @@ public class FlicksActivity extends AppCompatActivity {
 
             }
         });
-
     }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        isLandscape = newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE;
+        recyclerView.setAdapter(adapter);
+        adapter.orientationChanged(isLandscape);
+
+        Log.d("tag", "isLandscape: " + String.valueOf(isLandscape));
+    }
+
 }
--- app/src/main/java/com/example/ahirani/flicks/ui/FlicksAdapter.java
+++ app/src/main/java/com/example/ahirani/flicks/ui/FlicksAdapter.java
@@ -1,22 +1,74 @@
 package com.example.ahirani.flicks.ui;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.ahirani.flicks.R;
+import com.example.ahirani.flicks.ui.models.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 class FlicksAdapter extends RecyclerView.Adapter {
+    private static final String IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
+    private List<Movie> movies;
+    private boolean isLandscape;
+
+    public static class MovieViewHolder extends RecyclerView.ViewHolder {
+        TextView filmText;
+        TextView overviewText;
+        ImageView filmImage;
+
+        MovieViewHolder(View v) {
+            super(v);
+            filmText = v.findViewById(R.id.film_title_text);
+            overviewText = v.findViewById(R.id.film_overview_text);
+            filmImage = v.findViewById(R.id.film_thumbnail);
+        }
+    }
+
+    FlicksAdapter(List<Movie> movies, boolean isLandscape) {
+        this.movies = movies;
+        this.isLandscape = isLandscape;
+    }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.movie_item, parent, false);
+
+        return new MovieViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        MovieViewHolder movieViewHolder = (MovieViewHolder) holder;
+        movieViewHolder.filmText.setText(movies.get(position).getTitle());
+        movieViewHolder.overviewText.setText(movies.get(position).getOverviewText());
 
+        Context context = ((MovieViewHolder) holder).filmImage.getContext();
+
+        if (isLandscape) {
+            Log.d("tag", "backdrop");
+            Picasso.with(context).load(IMAGE_BASE_URL + movies.get(position).getBackdropPath()).into(movieViewHolder.filmImage);
+        } else {
+            Log.d("tag", "poster");
+            Picasso.with(context).load(IMAGE_BASE_URL + movies.get(position).getPosterPath()).into(movieViewHolder.filmImage);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return movies.size();
+    }
+
+    public void orientationChanged(boolean isLandscape) {
+        this.isLandscape = isLandscape;
     }
 }
